dd01a99a69c6acf4ee3643ffb2a35487a3efe2f1
Mateus <amaral5x@hotmail.com>
2022-11-17 20:00:55 -0300

    Aula 01

---
+++ inicio/aula1/conceito.js
@@ -0,0 +1,73 @@
+console.log("Hello, world")
+
+// Isto é uma linha de comentário
+
+/*
+    Isto é um comentário em bloco
+    pode utilizar várias
+    linhas
+
+*/
+
+/*
+* teste
+* teste 2
+*/
+
+{
+    console.log('Foi')
+console.log('Assim também')
+}
+
+console
+.log(
+    'Isso é uma sentença'
+    )
+
+console.
+log(
+    "Testando 1 2 3"
+)
+
+console
+
+.
+
+log ('uia')
+
+console.log('Teste'); console.log(Teste2)
+
+var numero = 1
+/* let
+const */
+console.log('Valor do número: ', numero)
+console.log(typeof numero)
+
+numero = "uma string"
+console.log('Valor do número', numero)
+console.log(typeof numero)
+
+// Declarando variáveis
+
+var a = 10
+let b = 15
+console.log(a, b)
+
+var a = 20
+b = 25
+console.log(a, b)
+
+const c = "Alunos"
+console.log(c)
+
+/*c = "Alunos"
+console.log(c)*/
+
+const nome = 'Gabriel'
+const num1 = 1.123
+const num2 = 2
+
+console.log(typeof nome)
+console.log(Number.isInteger(num1))
+console.log(Number.isInteger(num2))
+console.log(typeof num2)
