ce12b561839a117796ee3f6ac228b0e1dbb17dcf
Mateus <amaral5x@hotmail.com>
2022-11-22 18:47:30 -0300

    JS

--- inicio/aula1/conceito.js
+++ inicio/aula1/conceito.js
@@ -1,73 +1,91 @@
-console.log("Hello, world")
+//console.log("Hello, world!")
 
-// Isto é uma linha de comentário
+// Isto é um linha de comentário
 
-/*
+/* 
     Isto é um comentário em bloco
-    pode utilizar várias
+    pode utilizar varias
     linhas
+*/
 
+/* 
+ * teste
+ * asd
+ * hoje foi
+ * um dia
+ * que utilizei
+ * muitos 
+ * comentários
 */
 
 /*
-* teste
-* teste 2
-*/
-
 {
-    console.log('Foi')
-console.log('Assim também')
+    console.log("Foi")
+    console.log("Assim tbm")
 }
-
 console
 .log(
-    'Isso é uma sentença'
+    "Isto é uma sentença"
     )
+console.log("Testando 1 2 3 !!!!" )
+console.log("Teste"); 
+console.log("Foi 2")
+console.log('Aspas simples')
+console.log("Aspas duplas")
+*/
 
-console.
-log(
-    "Testando 1 2 3"
-)
+/* 
+* 3 formas de declarar variaveis
+* var
+* let
+* const
+*/
 
-console
+// var numero = 1
+// console.log("Valor do número: ", numero)
+// console.log(typeof numero)
 
-.
+// numero = "uma string" 
+// console.log("Valor do número: ", numero)
+// console.log(typeof numero)
 
-log ('uia')
 
-console.log('Teste'); console.log(Teste2)
+// Declarando variaveis
+// var a = 10
+// let b = 15
+// console.log(a, b)
 
-var numero = 1
-/* let
-const */
-console.log('Valor do número: ', numero)
-console.log(typeof numero)
+// var a = 20
+// b = 25
+// console.log(a, b)
 
-numero = "uma string"
-console.log('Valor do número', numero)
-console.log(typeof numero)
 
-// Declarando variáveis
+// const c = "Alunos"
+// console.log(c)
 
-var a = 10
-let b = 15
-console.log(a, b)
+// console.log(c)
 
-var a = 20
-b = 25
-console.log(a, b)
 
-const c = "Alunos"
-console.log(c)
-
-/*c = "Alunos"
-console.log(c)*/
-
-const nome = 'Gabriel'
-const num1 = 1.123
-const num2 = 2
+const nome = "Gabriel"
+const num1 = 4.0001
+const num2 = Number("5.1")
 
 console.log(typeof nome)
 console.log(Number.isInteger(num1))
 console.log(Number.isInteger(num2))
 console.log(typeof num2)
+
+
+
+/* 
+ * https://nodejs.org/en/ 
+ * 2 - Paginas
+ * Home / Contato
+ * Contato
+    - Formulário 
+        - Campo Nome 
+        - Email
+        - titulo da mensagem
+        - Descrição da mensagem 
+        - botão de envio
+ */
