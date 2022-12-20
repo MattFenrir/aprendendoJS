b7864f1ec9623462dfb45c328c17e22f63657343
Mateus <amaral5x@hotmail.com>
2022-12-20 20:34:56 -0300

    Aula 11

--- inicio/aula10/index.js
+++ inicio/aula10/index.js
@@ -1,84 +1,108 @@
 const url = 'https://pokeapi.co/api/v2/pokemon'
 const urlParams = new URLSearchParams(window.location.search)
-console.log(urlParams.get('name'))
-
 const getAllPokemon = async () => {
-    const response = await fetch(`${url}?limit=151`)
-    console.log(response)
-
-    const data = await response.json()
-    console.log(data)
-
-    for(let i = 0; i < data.results.length; i++){
-        const div = document.createElement('div')
-        const name = document.createElement('h2')
-        const link = document.createElement('a')
-
+    try{
+        const response = await fetch(`${url}?limit=151`)
+        const data = await response.json()
         removeView()
+        document.querySelector('#home #loading').classList.add('hide')
 
-        name.innerText = ` ${i + 1} ${data.results[i].name}`
-        link.setAttribute('href', `pokedex.html?name=${data.results[i].name}`)
-        link.innerText = 'ler mais'
+        data.results.map((pokemon, indice) => {
+            const div = document.createElement('div')
+            const name = document.createElement('h2')
+            const numero = document.createElement('p')
+            const img = document.createElement('img')
+            const link = document.createElement('a')
 
-        div.appendChild(name)
-        div.appendChild(link)
+            name.innerText = `${pokemon.name}`
+            numero.innerText = `N: ${++indice}º`
+            link.setAttribute('href', `pokedex.html?name=${pokemon.name}`)
+        
+            returnImg(pokemon.name).then((linkImg ) => { 
+                    img.setAttribute('src', linkImg )
+                }
+            )
 
-        document.querySelector('#content').appendChild(div)
+            div.appendChild(img)
+            div.appendChild(numero)
+            div.appendChild(name)
+            //div.appendChild(link)
+            link.appendChild(div)
+            document.querySelector('#content').appendChild(link)
+        })
+    }catch(e){
+        console.log(e.message)
     }
+
 }
 
 async function pokedex(){
-    const response = await fetch(`${url}/${urlParams.get('name')}/`)
-    const data = await response.json()
-    console.log(data)
-    
-    removeView()
+    try{
+        const response = await fetch(`${url}/${urlParams.get('name')}/`) 
+        const data = await response.json()
+        //console.log(data)
+        removeView()
 
-    const div = document.createElement('div')
-    const name = document.createElement('h2')
-    name.innerText = (data.name).toUpperCase()
+        const div = document.createElement('div')
+        const name = document.createElement('h2')
+        name.innerText = `${(data.name).toUpperCase()}`
+        const number = document.createElement('p')
+        const type = document.createElement('p')
+        const peso = document.createElement('p')
+        const altura = document.createElement('p')
+        const att = document.createElement('div')
 
-    const number = document.createElement('p')
-    number.innerText = `N: ${data.order}º`
+        number.innerText = ` N: ${data.order} º `
 
-    const weight = document.createElement('p')
-    weight.innerText = `Peso: ${(data.weight/10).toFixed(2)} kg`
-    
+        let types = ''
+        data.types.map((type) =>{
+            types += `<span class='${type.type.name}'>${type.type.name}</span> `
+        })
+        type.innerHTML += `Tipo: ${types}`
 
-    const height = document.createElement('p')
-    height.innerText = `Altura: ${(data.height/10).toFixed(2)} ${(data.height/10) < 1 ? 'cm' : 'm'}`
+        peso.innerText += `Peso: ${(data.weight/10).toFixed(1)} kg`
+        altura.innerText += `Altura: ${ (data.height/10) < 1 ? `${(data.height) * 10}  cm` : `${(data.height/10).toFixed(2)}  m`}`
 
-    const img = document.createElement('img')
-    img.setAttribute('src', data.sprites.front_default)
+        const img = document.createElement('img')
+        img.setAttribute('src', data.sprites.front_default) 
 
-    let moves = ''
-    for(let i in data.moves){
-        moves += `${data.moves[i].move.name}, `
+        let moves = ''
+        data.moves.map((move) =>{
+            moves += `<span>${move.move.name}</span> `
+        })
+        att.innerHTML = `<p class='attack'>Attacks:</p> ${moves}`
+
+        div.appendChild(name)
+        div.appendChild(img)
+        div.appendChild(number)
+        div.appendChild(type)
+        div.appendChild(peso)
+        div.appendChild(altura)
+        div.appendChild(att)
+        document.querySelector('#content').appendChild(div)
+        document.querySelector('#content').classList.add(`${data.types[0].type.name}`)
+
+    }catch(e){
+        console.log(e.message)
     }
-    moves = moves.substring(0, moves.length - 2)
-    
-    const att = document.createElement('p')
-    att.innerText = `Attacks: ${moves}.`
+}
 
-    const type = document.createElement('p')
-    type.innerText = `Tipo: ${data.types[0].type.name}`
-
-    div.appendChild(name)
-    div.appendChild(img)
-    div.appendChild(number)
-    div.appendChild(type)
-    div.appendChild(height)
-    div.appendChild(weight)
-    div.appendChild(att)
-
-    document.querySelector('#content').appendChild(div)
-
+const returnImg = async (name) => {
+    try{
+        const response = await fetch(`${url}/${name}/`) 
+        const data = await response.json()
+        return data.sprites.front_default
+        
+    }catch(e){
+        console.log(e.message)
+    }
 }
 
 function removeView(){
-    document.querySelector('#loading').classList.add('hide')
+    document.getElementById('loading').classList.add('hide')
     document.querySelector('#content').classList.remove('hide')
 }
 
-// Operador Ternário
+
+
 !urlParams.get('name') ? getAllPokemon() : pokedex()
